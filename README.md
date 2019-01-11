0f35d0adabbce4f47a5f039d7fe9c06c85180ed3
carldulo <cmduque@sura.com.co>
2019-01-11 11:28:21 -0500

    Actualizacion de los ejercicios corrigiendo errores de la base

--- README.md
+++ README.md
@@ -7,12 +7,21 @@
 
 ----
 ## Ejecicio 1
-1. Crear una función en AWS Lambda con doggos.js
-2. Descargar la página (doggos.html) y editar para que apunte a la función previamente creada
+1. Crear una función en AWS Lambda con doggos.zip
+2. Validar el funcionamiento
 
-> Archivos tomados de [github.com/spruke/doggos](https://github.com/spruke/doggos)
+> Archivos adaptados de [github.com/spruke/doggos](https://github.com/spruke/doggos)
 
 ----
 ## Ejecicio 2
+1. Agregar un trigger de API Gateway a la función creada
+2. Validar el funcionamiento a través del API Gateway
+3. Actualizar el contenido de la función con doggos.js
+4. Descargar la página Doggos.html, actualizar la URL para invocar la función creada y validar el funcionamiento
+
+> Archivos adaptados de [github.com/spruke/doggos](https://github.com/spruke/doggos)
+
+----
+## Ejecicio 3
 1. Crear una función en AWS Lambda que responda un sitio web
 2. Subir la página del ejercicio anterior y verificar que funciona correctamente
--- doggos.js
+++ doggos.js
@@ -4,29 +4,54 @@ let request = require('request');
 
 exports.handler = function(event, context, callback) {
 
-    let rating = event.rating;
-    let outOf = event.outOf;
+    let rating = event.queryStringParameters.rating;
+    let outOf = event.queryStringParameters.outOf;
 
     if (rating < 10) {
-        return callback("What are you asking for—some kind of... bad? dog? No such thing");
+        sendResponse(context, 200, '{ "error": "What are you asking for—some kind of... bad? dog? No such thing" }' );
+        return;
     }
 
     request('https://queryfeed.net/twitter?q='+rating+'%2F'+outOf+
             '+from%3Adog_rates&title-type=user-name-both&geocode=',
         function(error, response, body) {
             if (error) {
-                return callback("Error fetching Queryfeed:" + error);
+                console.log("Error fetching Queryfeed:" + error);
+                sendResponse(context, 200, '{ "error": "Error fetching Queryfeed" }' );
+                return;
             }
             parser.parseString(body, function(err, goodies) {
                 if (err) {
-                    return callback("Error parsing feed RSS:" + error);
+                    console.log("Error parsing feed RSS:" + error);
+                    sendResponse(context, 200, '{ "error": "Error parsing feed RSS" }' );
+                    return;
                 }
                 let doggoTweets = goodies.feed.entries;
                 if (doggoTweets.length == 0) {
-                    return callback("No doggos found at that level of goodness.");
+                    sendResponse(context, 200, '{ "error" : "No doggos found at that level of goodness." }' );
+                    return;
                 }
-                return callback(null, { tweet: _.sample(doggoTweets).link });
+                let index = Math.floor(Math.random() * doggoTweets.length);
+                //sendResponse(context, 200,  '{ "tweet" : "' + _.sample(doggoTweets).link + '"}' );
+                sendResponse(context, 200,  '{ "tweet" : "' + doggoTweets[index].link + '"}' );
+                return;
             });
         });
 
+}
+
+function sendResponse(context, statusCode, body) {
+  let response = {
+    'isBase64Encoded': 0,
+    'statusCode': statusCode,
+    'headers': {  
+       "Access-Control-Allow-Origin":"*",
+       "Access-Control-Allow-Methods":"DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
+       "Access-Control-Allow-Headers":"Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+       "Content-Type":"application/json"
+    },
+    'body': body
+  }
+  console.log(`Lambda exits.  Response sent with status code ${statusCode}`)
+  context.succeed(response)
 }
